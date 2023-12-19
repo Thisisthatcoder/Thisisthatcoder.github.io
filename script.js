ad72165b253e1529af3fc6caa7a1ae1538f75e79
Thisisthatcoder <87624277+Thisisthatcoder@users.noreply.github.com>
2023-12-19 06:53:18 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,19 +1,1 @@
-const prevBtn = document.querySelector('.prev');
-const nextBtn = document.querySelector('.next');
-const slides = document.querySelector('.slides');
-const slideWidth = document.querySelector('.slide').clientWidth;
-let slideIndex = 0;
 
-nextBtn.addEventListener('click', () => {
-  if (slideIndex < slides.children.length - 1) {
-    slideIndex++;
-    slides.style.transform = `translateX(${-slideIndex * slideWidth}px)`;
-  }
-});
-
-prevBtn.addEventListener('click', () => {
-  if (slideIndex > 0) {
-    slideIndex--;
-    slides.style.transform = `translateX(${-slideIndex * slideWidth}px)`;
-  }
-});

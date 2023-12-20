3a21421e4b27d9801738a0bf9344bb3af59a2781
Thisisthatcoder <87624277+Thisisthatcoder@users.noreply.github.com>
2023-12-19 22:00:55 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 
+document.getElementById('subscriptionForm').addEventListener('submit', function(event) {
+    event.preventDefault(); // Prevent the default form submission
+  
+    // Hide form and header
+    document.getElementById('subscriptionForm').style.display = 'none';
+    document.querySelector('.subscription h1').style.display = 'none';
+    document.querySelector('.subscription h2').style.display = 'none';
+  
+    // Display thank you message
+    document.getElementById('thankYouMessage').style.display = 'block';
+  });
